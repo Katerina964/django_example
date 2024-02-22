1f6bebe6a56ab163b0d9baf10152b1f20ec75c26
Katerina964 <katrin.balakina@gmail.com>
2024-02-22 21:15:35 +0200

    orm

--- src/project/example_app/views.py
+++ src/project/example_app/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from example_app.models import Choice
 
 
 def index(request):
+   
+    (Choice(choice_text="What's new?", votes=1)).save()
     return HttpResponse("Hello, world WITH LOVE")
